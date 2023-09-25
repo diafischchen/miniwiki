cccf28f6bfef88f81a72c138042206c6ca7b464f
diafischchen <me@diafischchen.net>
2023-09-25 15:34:05 +0200

    Modal animation

--- view/wiki/images/upload-modal.php
+++ view/wiki/images/upload-modal.php
@@ -2,7 +2,7 @@
     <div class="modal-box">
         <div class="modal-header">
             Test Modal Box
-            <button class="modal-close-button font-lg modal-closer"><span class="icon-close"></span></button>
+            <button class="modal-close-button font-lg modal-closer"><i class="fa-solid fa-xmark"></i></button>
         </div>
         <div class="modal-content">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut eveniet nam dicta laudantium alias quae saepe accusantium illo rerum perferendis!
